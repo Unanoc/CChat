c8e5b5aed04066b4082baf101f23e802bc9400e2
Unanoc <leedaniil98@gmail.com>
2019-03-01 14:19:59 +0300

    added unregister channel and description for documentation

--- pkg/server/room.go
+++ pkg/server/room.go
@@ -4,13 +4,13 @@ import (
 	"net"
 )
 
-// Client ...
+// Client is used to get access to connection
 type Client struct {
 	Username string
 	Conn     net.Conn
 }
 
-// CreateClient ...
+// CreateClient returns an instance of Client
 func CreateClient(username string, conn net.Conn) *Client {
 	return &Client{
 		Username: username,
@@ -18,52 +18,54 @@ func CreateClient(username string, conn net.Conn) *Client {
 	}
 }
 
-// CreateRoom ...
+// CreateRoom returns an instance of Room
 func CreateRoom(name string) *Room {
 	return &Room{
-		Name:     name,
-		Register: make(chan net.Conn),
-		Clients:  make(map[string]*Client),
-		Messages: make(chan string),
+		Name:       name,
+		Register:   make(chan *Client),
+		Unregister: make(chan *Client),
+		Clients:    make(map[string]*Client),
+		Messages:   make(chan string),
 	}
 }
 
-// Room ...
+// Room is manager of actions of clients
 type Room struct {
-	Name     string
-	Register chan net.Conn
-	Clients  map[string]*Client
-	Messages chan string
+	Name       string
+	Register   chan *Client
+	Unregister chan *Client
+	Clients    map[string]*Client
+	Messages   chan string
 }
 
-// Run ...
+// Run starts a room for message exchange
 func (r *Room) Run() {
 	for {
 		select {
 		case msg := <-r.Messages:
-			r.broadcast(msg)
+			r.Broadcast(msg)
+		case client := <-r.Register:
+			r.Clients[client.Username] = client
+			r.Broadcast(client.Username + " joined to the room")
+		case client := <-r.Unregister:
+			client.Conn.Close()
+			delete(r.Clients, client.Username)
+			r.Broadcast(client.Username + "left the room")
 		}
 	}
 }
 
-// Broadcast ...
-func (r *Room) broadcast(msg string) {
+// Broadcast sends messages for all clients in the room
+func (r *Room) Broadcast(msg string) {
 	for _, client := range r.Clients {
 		_, err := client.Conn.Write([]byte(msg))
 		if err != nil {
-			r.RemoveClient(client.Username)
+			r.Unregister <- client
 		}
 	}
 }
 
-// ClientCount ...
+// ClientCount returns count of clients
 func (r *Room) ClientCount() int {
 	return len(r.Clients)
-}
-
-// RemoveClient ...
-func (r *Room) RemoveClient(username string) {
-	if client, ok := r.Clients[username]; ok {
-		delete(r.Clients, client.Username)
-	}
 }
