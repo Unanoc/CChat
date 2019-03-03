9d119ccc53e64936ae1d91d4c31b00fdece80a7d
Unanoc <leedaniil98@gmail.com>
2019-03-03 22:25:26 +0300

    refactoring of client layer

--- cmd/chat-client/main.go
+++ cmd/chat-client/main.go
@@ -2,30 +2,20 @@ package main
 
 import (
 	"fmt"
-	"net"
 
 	"chat/pkg/client"
-
-	"github.com/fatih/color"
 )
 
 var (
-	host   = "127.0.0.1"
-	port   = "8000"
-	remote = host + ":" + port
+	host = "127.0.0.1"
+	port = "8000"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", remote)
-	defer conn.Close()
+	c, err := client.CreateClient(host, port)
+	defer c.Disconnect()
 
 	if err != nil {
-		color.Red("Server not found")
-		return
-	}
-
-	c := client.CreateClient(conn)
-	if err := c.ProcessJoin(); err != nil {
 		fmt.Print(err)
 		return
 	}
--- pkg/client/client.go
+++ pkg/client/client.go
@@ -10,52 +10,39 @@ import (
 )
 
 // CreateClient returns an instance of Client
-func CreateClient(conn net.Conn) *Client {
-	return &Client{
-		Conn: conn,
+func CreateClient(host, port string) (*Client, error) {
+	client := &Client{
+		Host:   host,
+		Port:   port,
+		Remote: host + ":" + port,
 	}
+
+	conn, err := net.Dial("tcp", client.Remote)
+	if err != nil {
+		return nil, fmt.Errorf(color.RedString("Server not found"))
+	}
+	client.Conn = conn
+
+	return client, nil
 }
 
 // Client keeps the connection of user
 type Client struct {
-	Conn net.Conn
+	Conn   net.Conn
+	Host   string
+	Port   string
+	Remote string
 }
 
-// ProcessJoin organizes the connection process
-func (c *Client) ProcessJoin() error {
-	// Getting the client's name and room's name
-	for i := 0; i < 2; i++ {
-		if err := ReadAndWrite(c); err != nil {
-			return err
-		}
-	}
-	color.HiGreen("You have been successfully connected")
-	return nil
-}
-
-// ReadAndWrite accepts request message from server and sends client's reply to server
-func ReadAndWrite(client *Client) error {
-	writeStr, readStr := make([]byte, 254), make([]byte, 254)
-
-	// Getting from server
-	length, err := client.Conn.Read(readStr)
-	if err != nil {
-		return fmt.Errorf(color.RedString("Error when recieve from server"))
-	}
-	fmt.Printf("%s", readStr[:length])
-
-	// Writing to server
-	fmt.Scanf("%s", &writeStr)
-	if _, err := client.Conn.Write([]byte(writeStr)); err != nil {
-		return fmt.Errorf(color.RedString("Error when send to server"))
-	}
-
-	return nil
+// Disconnect closes client's connection
+func (c *Client) Disconnect() {
+	c.Conn.Close()
 }
 
 // GetMessagesHandler gets messages for client
 func (c *Client) GetMessagesHandler() {
 	readStr := make([]byte, 254)
+
 	for {
 		length, err := c.Conn.Read(readStr)
 		if err != nil {
@@ -69,6 +56,7 @@ func (c *Client) GetMessagesHandler() {
 // WriteMessagesHandler sends client's messages
 func (c *Client) WriteMessagesHandler() {
 	reader := bufio.NewReader(os.Stdin)
+
 	for {
 		writeStr, _, _ := reader.ReadLine()
 		_, err := c.Conn.Write([]byte(writeStr))
